da673df9f1da590bda92884afbe89f7cf84e016a
Aleksandra Gierczak <aleksandra.gierczak09@gmail.com>
2023-02-06 13:30:00 +0100

    docstring added

--- 1_Functional_Programming/1_EASY/4_find_in_list/find_in_list.py
+++ 1_Functional_Programming/1_EASY/4_find_in_list/find_in_list.py
@@ -1,7 +1,23 @@
 from collections import namedtuple
 
 
-def find_item(list_, key_):
+def find_item(list_: list, key_: object):
+    """Return namedtuple with item and its index if key parameter is found in
+    searched list. Return empty tuple if key parameter is not in searched list.
+
+    Parameters
+    ----------
+    list_ : list
+        A list searched through to find a key parameter
+    key_ : object
+        The key parameter looked for in the list
+
+    Returns
+    -------
+    tuple
+        namedtuple if key_ is in the list_, empty tuple otherwise
+    """
+
     result_tuple = namedtuple(typename="Result", field_names=["value", "index"])
     for index, value_ in enumerate(list_):
         if key_ == value_:
