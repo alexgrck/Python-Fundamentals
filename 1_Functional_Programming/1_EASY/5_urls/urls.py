d09dd6511db8428e9c9efe72c593c48c54dc1bac
Aleksandra Gierczak <aleksandra.gierczak09@gmail.com>
2023-02-06 14:34:10 +0100

    doctrings added

--- 1_Functional_Programming/1_EASY/5_urls/urls.py
+++ 1_Functional_Programming/1_EASY/5_urls/urls.py
@@ -2,11 +2,36 @@ from urllib.parse import urlparse
 from tldextract import tldextract
 
 
-def shrunk_url(url_string):
+def shrunk_url(url_string: str):
+    """Return URL address without https:// or http://, depending on which
+    protocol is in the address.
+
+    Parameters
+    ----------
+    url_string : str
+        An URL to be shrunked
+
+    Returns
+    -------
+    str
+        An URL string without HTTP/HTTPS protocol.
+    """
     return url_string.split("//")[1]
 
 
-def domain_of_url(url_string):
+def domain_of_url(url_string: str):
+    """Return a main domain from given URL.
+
+    Parameters
+    ----------
+    url_string : str
+        An URL from which main domain will be extracted.
+
+    Returns
+    -------
+    str
+        Concatenated strings of URL domain and suffix.
+    """
     return (
         tldextract.extract(url_string).domain
         + "."
@@ -14,7 +39,19 @@ def domain_of_url(url_string):
     )
 
 
-def url_list(url_string):
+def url_list(url_string: str):
+    """Return a list of elements of URL.
+
+    Parameters
+    ----------
+    url_string : str
+        An URL string to be splitted into a list of elements.
+
+    Returns
+    -------
+    list
+        List of elements of an URL parameter.
+    """
     result = (
         [urlparse(url_string).scheme + "://"]
         + urlparse(url_string).netloc.split(".")
