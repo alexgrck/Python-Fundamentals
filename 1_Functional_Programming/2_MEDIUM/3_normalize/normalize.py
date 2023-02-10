2f5c14c564f8755d89d31caef674c65b5b4e494e
Aleksandra Gierczak <aleksandra.gierczak09@gmail.com>
2023-02-10 12:28:10 +0100

    docstrings added

--- 1_Functional_Programming/2_MEDIUM/3_normalize/normalize.py
+++ 1_Functional_Programming/2_MEDIUM/3_normalize/normalize.py
@@ -4,12 +4,36 @@ data = [random.randint(0, 100000) for x in range(400)]
 
 
 def normalization_0_1_closed(list_):
+    """Return normalized data from list given as parameter of the function.
+
+    Parameters
+    ----------
+    list_
+        A list of integers to normalize
+
+    Returns
+    list
+        A list of normalized data in range from 0 to 1, where 0 is minimum and
+        1 is maximum.
+    """
     if max(list_) == min(list_):
         return 0.5
     return sorted([(el - min(list_)) / (max(list_) - min(list_)) for el in list_])
 
 
 def normalization_0_1_open(list_):
+    """Return normalized data from list given as parameter of the function.
+
+    Parameters
+    ----------
+    list_
+        A list of integers to normalize
+
+    Returns
+    list
+        A list of normalized data in range from 0 to 1, where 0 is minimum and
+        1 is never reached.
+    """
     if max(list_) == min(list_):
         return 0.5
     return sorted([(el - min(list_)) / ((max(list_) + 1) - min(list_)) for el in list_])
