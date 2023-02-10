9a545922f865bd586a25239c4bf6d21a061ce2fd
Aleksandra Gierczak <aleksandra.gierczak09@gmail.com>
2023-02-10 11:56:08 +0100

    docstrings added

--- 1_Functional_Programming/2_MEDIUM/1_args_and_other_kwargs/args_and_other_kwargs.py
+++ 1_Functional_Programming/2_MEDIUM/1_args_and_other_kwargs/args_and_other_kwargs.py
@@ -1,2 +1,17 @@
 def list_parameters(*args, **kwargs):
+    """Return a dictionary of arguments and keyword arguments from given any
+    number of arguments and keywors arguments.
+
+    Parameters
+    ----------
+    *args
+        Any number of given arguments
+    **kwargs
+        Any number of given keywords arguments
+
+    Returns
+    -------
+    dict
+        A dictionary of arguments and keyword arguments
+    """
     return {**{index: value for index, value in enumerate(args)}, **kwargs}
