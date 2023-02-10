3d989504c5ecb994ba54e4628383df8dcef96e7c
Aleksandra Gierczak <aleksandra.gierczak09@gmail.com>
2023-02-10 12:02:30 +0100

    docstrings added

--- 1_Functional_Programming/2_MEDIUM/2_infinite_randoms/infinite_randoms.py
+++ 1_Functional_Programming/2_MEDIUM/2_infinite_randoms/infinite_randoms.py
@@ -4,6 +4,18 @@ history = []
 
 
 def random_number():
+    """Returns a list of generated random numbers. Numbers are stored in
+    history list.
+
+    Parametes
+    ---------
+    None
+
+    Returns
+    -------
+    generator
+        A generator of random numbers
+    """
     global history
     while True:
         number = random.randint(1, 10000)
