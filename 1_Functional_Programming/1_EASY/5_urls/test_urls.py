d2723ef2c2c6069fde2d8d147f610941a9864104
Aleksandra Gierczak <aleksandra.gierczak09@gmail.com>
2023-02-10 11:49:02 +0100

    docstrings added

--- 1_Functional_Programming/1_EASY/5_urls/test_urls.py
+++ 1_Functional_Programming/1_EASY/5_urls/test_urls.py
@@ -3,29 +3,58 @@ from urls import shrunk_url, domain_of_url, url_list
 
 
 class TestCountDays(unittest.TestCase):
- 
     def test_shrunk_url(self):
-        test_url_1 = 'http://amazon.co.uk'
-        test_url_2 = 'https://amazon.co.uk'
-        test_url_3 = 'ftp://public.ftp-servers.example.com'
-        self.assertEqual(shrunk_url(test_url_1), 'amazon.co.uk')
-        self.assertEqual(shrunk_url(test_url_2), 'amazon.co.uk')
-        self.assertEqual(shrunk_url(test_url_3), 'public.ftp-servers.example.com')
-    
+        """Test if function shrunk_url() correctly shrunk https:// or http://
+        from given URL.
+        """
+        test_url_1 = "http://amazon.co.uk"
+        test_url_2 = "https://amazon.co.uk"
+        test_url_3 = "ftp://public.ftp-servers.example.com"
+        self.assertEqual(shrunk_url(test_url_1), "amazon.co.uk")
+        self.assertEqual(shrunk_url(test_url_2), "amazon.co.uk")
+        self.assertEqual(shrunk_url(test_url_3), "public.ftp-servers.example.com")
+
     def test_domain_of_url(self):
-        test_url_1 = 'https://example.amazon.co.uk'
-        test_url_2 = 'ftp://public.ftp-servers.example.com'
-        self.assertEqual(domain_of_url(test_url_1), 'amazon.co.uk')
-        self.assertEqual(domain_of_url(test_url_2), 'example.com')
+        """Test if function domain_of_url() correctly returns main domain from
+        given URL.
+        """
+        test_url_1 = "https://example.amazon.co.uk"
+        test_url_2 = "ftp://public.ftp-servers.example.com"
+        self.assertEqual(domain_of_url(test_url_1), "amazon.co.uk")
+        self.assertEqual(domain_of_url(test_url_2), "example.com")
 
     def test_url_list(self):
-        test_url_1 = 'https://www.programiz.com/python-programming/global-keyword'
-        test_url_2 = 'ftp://public.ftp-servers.example.com'
-        test_url_3 = 'https://www.gov.uk/government/statistical-data-sets/unclaimed-estates-list'
-        self.assertEqual(url_list(test_url_1), ['https://', 'www', 'programiz', 
-            'com', 'python-programming', 'global-keyword'])
-        self.assertEqual(url_list(test_url_2), ['ftp://', 'public', 'ftp-servers', 
-            'example', 'com'])
-        self.assertEqual(url_list(test_url_3), ['https://', 'www', 'gov', 
-            'uk', 'government', 'statistical-data-sets', 'unclaimed-estates-list'])
-
+        """Test if function url_list() correctly returns list of elemnts of
+        given URL.
+        """
+        test_url_1 = "https://www.programiz.com/python-programming/global-keyword"
+        test_url_2 = "ftp://public.ftp-servers.example.com"
+        test_url_3 = (
+            "https://www.gov.uk/government/statistical-data-sets/unclaimed-estates-list"
+        )
+        self.assertEqual(
+            url_list(test_url_1),
+            [
+                "https://",
+                "www",
+                "programiz",
+                "com",
+                "python-programming",
+                "global-keyword",
+            ],
+        )
+        self.assertEqual(
+            url_list(test_url_2), ["ftp://", "public", "ftp-servers", "example", "com"]
+        )
+        self.assertEqual(
+            url_list(test_url_3),
+            [
+                "https://",
+                "www",
+                "gov",
+                "uk",
+                "government",
+                "statistical-data-sets",
+                "unclaimed-estates-list",
+            ],
+        )
