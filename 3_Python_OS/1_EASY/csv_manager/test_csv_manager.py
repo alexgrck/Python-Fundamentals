efeec733d6ea4093e4a2df4b2670bb52c0a228d3
Aleksandra Gierczak <aleksandra.gierczak09@gmail.com>
2023-03-01 14:00:49 +0100

    feat: add some new data, add new testcase to one of test methods fix: tearDown, test_add_more_rows

--- 3_Python_OS/1_EASY/csv_manager/test_csv_manager.py
+++ 3_Python_OS/1_EASY/csv_manager/test_csv_manager.py
@@ -29,6 +29,14 @@ class TestCSVManager(TestCase):
                 " NJ",
                 "8075",
             ],
+            [
+                "Stephen",
+                "Tyler",
+                '7452 Terrace "At the Plaza" road',
+                "SomeTown",
+                "SD",
+                "91234",
+            ],
         ]
         self.fieldnames = ["Name", "Surname", "Address", "City", "State", "Income"]
         self.test_dicts = [
@@ -56,13 +64,21 @@ class TestCSVManager(TestCase):
                 "State": " NJ",
                 "Income": "8075",
             },
+            {
+                "Name": "Stephen",
+                "Surname": "Tyler",
+                "Address": '7452 Terrace "At the Plaza" road',
+                "City": "SomeTown",
+                "State": "SD",
+                "Income": "91234",
+            },
         ]
 
     def tearDown(self):
-        if self.csv_m1.abs_path.exists():
-            self.csv_m1.abs_path.unlink()
-        elif self.csv_m2.abs_path.exists():
+        if self.csv_m2.abs_path.exists():
             self.csv_m2.abs_path.unlink()
+        elif self.csv_m1.abs_path.exists():
+            self.csv_m1.abs_path.unlink()
 
     def test_create_file(self):
         self.csv_m1.create_file(self.csv_m1.abs_path)
@@ -84,54 +100,40 @@ class TestCSVManager(TestCase):
             row=self.test_dicts[0],
         )
         final_data1 = self.test_data[:2]
-        self.csv_m2.add_rows_with_header(
-            fieldnames=self.fieldnames, rows=self.test_dicts
-        )
         with open(self.csv_m1.abs_path, "r", newline="") as file:
             reader = [line for line in csv.reader(file)]
             self.assertEqual(reader, final_data1)
+        self.csv_m2.add_rows_with_header(
+            fieldnames=self.fieldnames, rows=self.test_dicts
+        )
         with open(self.csv_m2.abs_path, "r", newline="") as file:
             reader = [line for line in csv.reader(file)]
             self.assertEqual(reader, self.test_data)
 
-    # def test_add_rows_with_header(self):
-    #     self.csv_m.create_file(self.csv_m.abs_path)
-    #     self.csv_m.add_rows_with_header(
-    #         fieldnames=self.fieldnames, rows=self.test_dicts
-    #     )
-    #     with open(self.csv_m.abs_path, "r", newline="") as file:
-    #         reader = [line for line in csv.reader(file)]
-    #         self.assertEqual(reader, self.test_data)
+    def test_add_more_rows(self):
+        self.csv_m_with_data.add_more_rows(
+            fieldnames=self.fieldnames,
+            row=self.test_dicts[1],
+        )
+        with open(self.csv_m_with_data.abs_path, "r", newline="") as file:
+            reader = [line for line in csv.reader(file)]
+            final_data = self.test_data[:3]
+            self.assertEqual(reader, final_data)
 
-    # def add_one_more_row(self):
-    #     self.csv_m.create_file(self.csv_m.abs_path)
-    #     self.csv_m.add_rows_with_header(
-    #         fieldnames=self.fieldnames,
-    #         row=self.test_dicts[0],
-    #     )
-    #     self.csv_m.add_more_rows(
-    #         fieldnames=self.fieldnames,
-    #         row=self.test_dicts[1],
-    #     )
-    #     with open(self.csv_m.abs_path, "r", newline="") as file:
-    #         reader = [line for line in csv.reader(file)]
-    #         final_data = self.test_data[:3]
-    #         self.assertEqual(reader, final_data)
-
-    # def add_more_rows(self):
-    #     self.csv_m.create_file(self.csv_m.abs_path)
-    #     self.csv_m.add_rows_with_header(
-    #         fieldnames=self.fieldnames,
-    #         row=self.test_dicts[0],
-    #     )
-    #     self.csv_m.add_more_rows(
-    #         fieldnames=self.fieldnames,
-    #         rows=self.test_dicts[:2],
-    #     )
-    #     with open(self.csv_m.abs_path, "r", newline="") as file:
-    #         reader = [line for line in csv.reader(file)]
-    #         # final_data = self.test_data
-    #         self.assertEqual(reader, self.test_data)
+        with open(self.csv_m_with_data.abs_path, "w", newline="") as file:
+            writer = csv.writer(file, delimiter=",")
+            writer.writerows(self.test_data[:2])
+        self.csv_m_with_data.add_more_rows(
+            fieldnames=self.fieldnames,
+            rows=self.test_dicts[1:3],
+        )
+        with open(self.csv_m_with_data.abs_path, "r", newline="") as file:
+            reader = [line for line in csv.reader(file)]
+            final_data = self.test_data[:4]
+            self.assertEqual(reader, final_data)
+        with open(self.csv_m_with_data.abs_path, "w", newline="") as file:
+            writer = csv.writer(file, delimiter=",")
+            writer.writerows(self.test_data[:2])
 
     # # def test_update_file(self):
     # #     pass
