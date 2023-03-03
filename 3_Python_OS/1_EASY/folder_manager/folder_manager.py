838157bad722cd0e833cb249c1a38f7bf2df4399
Aleksandra Gierczak <aleksandra.gierczak09@gmail.com>
2023-03-03 12:46:25 +0100

    feat: all methods refactored

--- 3_Python_OS/1_EASY/folder_manager/folder_manager.py
+++ 3_Python_OS/1_EASY/folder_manager/folder_manager.py
@@ -1,41 +1,50 @@
 import shutil
-from pathlib import Path
 from warnings import warn
+import os
 
 
 class FolderManager:
     def __init__(self, path_string):
-        self.file_path = Path(path_string)
         self.abs_path = (
-            self.file_path
-            if self.file_path.is_absolute()
-            else Path.cwd().joinpath(self.file_path)
+            os.path.normpath(path_string)
+            if os.path.isabs(path_string)
+            else os.path.abspath(path_string)
         )
 
     def create_folders(self, *directories):
-        path = self.abs_path.joinpath(*directories)
-        if not Path.exists(path):
-            Path.mkdir(path, parents=True)
+        path = os.path.join(self.abs_path, *directories)
+        if not os.path.exists(path):
+            os.makedirs(path, exist_ok=True)
 
     def list_content_of_folder(self, folder_path):
-        f_path = Path(folder_path)
-        return [path_object for path_object in f_path.iterdir()]
+        f_path = (
+            os.path.normpath(folder_path)
+            if os.path.isabs(folder_path)
+            else os.path.abspath(folder_path)
+        )
+        return [
+            folder for root, folders, files in os.walk(f_path) for folder in folders
+        ]
 
     def delete_folders(self, path):
-        path_pathlib = Path(path)
+        f_path = (
+            os.path.normpath(path) if os.path.isabs(path) else os.path.abspath(path)
+        )
         try:
-            path_pathlib.rmdir()
+            os.rmdir(f_path)
         except OSError:
             warn("The folder is not empty!")
             input_ = input("Do you want to delete all folders in this directory? Y/N ")
-            if input_ == "y" or input_ == "Y":
-                shutil.rmtree(path_pathlib)
+            if input_ in ("y", "Y"):
+                shutil.rmtree(f_path)
 
     def create_folder_tree(self, tree_dict, path=None):
         for key, val in tree_dict.items():
             new_path = (
-                self.abs_path.joinpath(key) if path is None else path.joinpath(key)
+                os.path.join(self.abs_path, key)
+                if path is None
+                else os.path.join(path, key)
             )
-            Path.mkdir(new_path)
+            os.makedirs(new_path)
             if isinstance(val, dict):
                 self.create_folder_tree(tree_dict=val, path=new_path)
