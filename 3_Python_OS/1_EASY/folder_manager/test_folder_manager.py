0f895d476758fa7f2855753e2085fffe461a772a
Aleksandra Gierczak <aleksandra.gierczak09@gmail.com>
2023-03-06 13:58:06 +0100

    feat: all test methods refactored

--- 3_Python_OS/1_EASY/folder_manager/test_folder_manager.py
+++ 3_Python_OS/1_EASY/folder_manager/test_folder_manager.py
@@ -1,74 +1,55 @@
 import os
 import shutil
-from pathlib import WindowsPath
 from unittest import TestCase
 from unittest.mock import patch
+from itertools import chain
 
 from folder_manager import FolderManager
 
 
 class TestFolderManager(TestCase):
     def setUp(self):
-        self.fm = FolderManager(
-            "C://Ola//LocalHost//python-fundamentals-master//3_Python_OS//1_EASY//folder_manager",
-        )
+        self.fm = FolderManager("5_test_folder")
+        self.full_path = os.path.join(os.getcwd(), "5_test_folder/test_1/test2/test3")
+        self.middle_path = os.path.join(os.getcwd(), "5_test_folder/test_1/test2")
 
     def tearDown(self):
-        try:
-            for root, dirs, files in os.walk(self.fm.abs_path):
-                for name in dirs:
-                    os.rmdir(os.path.join(root, name))
-        except OSError:
-            for root, dirs, files in os.walk(self.fm.abs_path):
-                for name in dirs:
-                    shutil.rmtree(os.path.join(root, name))
+        shutil.rmtree(os.path.join(os.getcwd(), "5_test_folder"))
 
     def test_create_folders(self):
-        self.fm.create_folders("folder1", "folder2")
-        folders_path = self.fm.abs_path.joinpath("folder1", "folder2")
-        self.assertTrue(folders_path)
+        self.fm.create_folders("test_1/test2/test3")
+        self.assertTrue(os.path.exists(self.full_path))
 
     def test_list_content_of_folder(self):
-        list_content = self.fm.list_content_of_folder(
-            r"C:\Ola\LocalHost\python-fundamentals-master\3_Python_OS\1_EASY\folder_manager"
+        os.makedirs(self.full_path)
+        path = os.path.join(os.getcwd(), "5_test_folder")
+        list_content = self.fm.list_content_of_folder(path)
+        result_list = list(
+            chain.from_iterable([dirs for root, dirs, files in os.walk(path)])
         )
-        test_list = [
-            WindowsPath(
-                "C:/Ola/LocalHost/python-fundamentals-master/3_Python_OS/1_EASY/folder_manager/folder_manager.py"
-            ),
-            WindowsPath(
-                "C:/Ola/LocalHost/python-fundamentals-master/3_Python_OS/1_EASY/folder_manager/folder_manager_with_comments.py"
-            ),
-            WindowsPath(
-                "C:/Ola/LocalHost/python-fundamentals-master/3_Python_OS/1_EASY/folder_manager/test_folder_manager.py"
-            ),
-        ]
-
-        self.assertEqual(list_content, test_list)
+        self.assertEqual(list_content, result_list)
 
     def test_delete_1_folder(self):
-        self.fm.create_folders("folder1", "folder2")
-        folder_path = self.fm.abs_path.joinpath("folder1", "folder2")
-        path_after_deletion = self.fm.abs_path.joinpath("folder1")
-        self.fm.delete_folders(folder_path)
-        self.assertTrue(path_after_deletion)
+        os.makedirs(self.full_path)
+        self.fm.delete_folders(self.full_path)
+        self.assertFalse(os.path.exists(self.full_path))
+        self.assertTrue(os.path.exists(self.middle_path))
 
     @patch("builtins.input", return_value="y")
     def test_delete_folder_with_subdirectories_yes_input(self, mocked_input):
-        self.fm.create_folders("folder1", "folder2")
-        folder_path = self.fm.abs_path.joinpath("folder1")
+        os.makedirs(self.full_path)
         with self.assertWarns(Warning):
-            self.fm.delete_folders(folder_path)
-        self.assertTrue(folder_path)
+            self.fm.delete_folders(self.middle_path)
+        path_after_deletion = os.path.join(os.getcwd(), "5_test_folder/test_1")
+        self.assertFalse(os.path.exists(self.full_path))
+        self.assertTrue(os.path.exists(path_after_deletion))
 
     @patch("builtins.input", return_value="n")
     def test_delete_folder_with_subdirectories_no_input(self, mocked_input):
-        self.fm.create_folders("folder1", "folder2")
-        folder_path = self.fm.abs_path.joinpath("folder1", "folder2")
-        folder_to_delete = self.fm.abs_path.joinpath("folder1")
+        os.makedirs(self.full_path)
         with self.assertWarns(Warning):
-            self.fm.delete_folders(folder_to_delete)
-        self.assertTrue(folder_path)
+            self.fm.delete_folders(self.middle_path)
+        self.assertTrue(os.path.exists(self.full_path))
 
     def test_create_folder_tree(self):
         test_dict = {
@@ -76,11 +57,15 @@ class TestFolderManager(TestCase):
             "second": {"1stfolder": {"2nd folder": {"file": "file.csv"}}},
         }
         self.fm.create_folder_tree(test_dict)
-        path_first = self.fm.abs_path.joinpath(
-            "first", "1stfolder", "2ndfolder", "3rdfolder", "file"
+        path_first = os.path.join(
+            os.getcwd(), "first/1stfolder/2ndfolder/3rdfolder/file"
         )
-        path_second = self.fm.abs_path.joinpath(
-            "second", "1stfolder", "2ndfolder", "file", "file.csv"
+        path_second = os.path.join(
+            os.getcwd(), "second/1stfolder/2ndfolder/file/file.csv"
         )
-        self.assertTrue(path_first)
-        self.assertTrue(path_second)
+        for root, dirs, files in os.walk(path_first):
+            self.assertTrue(dirs)
+            self.assertTrue(files)
+        for root, dirs, files in os.walk(path_second):
+            self.assertTrue(dirs)
+            self.assertTrue(files)
