375c242054e16fef4f0997cc71e3a96fa1e17c97
Aleksandra Gierczak <aleksandra.gierczak09@gmail.com>
2023-03-15 14:16:34 +0100

    all test methods changed

--- 3_Python_OS/1_EASY/csv_manager/test_csv_manager.py
+++ 3_Python_OS/1_EASY/csv_manager/test_csv_manager.py
@@ -1,19 +1,17 @@
 from unittest import TestCase
 from csv_manager import CSVManager
 import csv
-from pathlib import WindowsPath
+from pathlib import Path
+import shutil
+import os
 
 
 class TestCSVManager(TestCase):
     def setUp(self):
-        self.csv_m1 = CSVManager(
-            "C:\\Ola\\LocalHost\\python-fundamentals-master\\3_Python_OS\\1_EASY\\csv_manager",
-            "test_csv1.csv",
-        )
-        self.csv_m2 = CSVManager(
-            "C:\\Ola\\LocalHost\\python-fundamentals-master\\3_Python_OS\\1_EASY\\csv_manager",
-            "test_csv2.csv",
-        )
+        self.test_manager = CSVManager("test_folder", file_name="test_file.csv")
+        self.file_path = os.path.join(os.getcwd(), "test_folder", "test_file.csv")
+        self.folder_path = os.path.join(os.getcwd(), "test_folder")
+        self.folder_path_pathlib = Path(self.folder_path)
 
         self.test_data = [
             ["Name", "Surname", "Address", "City", "State", "Income"],
@@ -73,103 +71,109 @@ class TestCSVManager(TestCase):
         ]
 
     def tearDown(self):
-        if self.csv_m2.abs_path.exists():
-            self.csv_m2.abs_path.unlink()
-        elif self.csv_m1.abs_path.exists():
-            self.csv_m1.abs_path.unlink()
+        if os.path.exists(self.folder_path):
+            shutil.rmtree(self.folder_path)
+        elif os.path.exists(self.file_path):
+            os.remove(self.file_path)
 
     def test_create_file(self):
-        self.csv_m1.create_file(self.csv_m1.abs_path)
-        self.assertTrue(self.csv_m1.abs_path.exists())
+        Path.mkdir(self.folder_path_pathlib, parents=True)
+        self.test_manager.create_file(self.file_path)
+        self.assertTrue(os.path.exists(self.file_path))
 
     def test_read_file(self):
-        self.csv_m1.create_file(self.csv_m1.abs_path)
-        with open(self.csv_m1.abs_path, "w", newline="") as file:
+        Path.mkdir(self.folder_path_pathlib, parents=True)
+        with open(self.file_path, "w+", newline="") as file:
             writer = csv.writer(file, delimiter=",")
             writer.writerows(self.test_data)
-        read = self.csv_m1.read_csv()
+        read = self.test_manager.read_csv()
         self.assertEqual(self.test_data, read)
 
     def test_add_rows_with_header(self):
-        self.csv_m1.create_file(self.csv_m1.abs_path)
-        self.csv_m2.create_file(self.csv_m2.abs_path)
-        self.csv_m1.add_rows_with_header(
-            fieldnames=self.fieldnames,
-            row=self.test_dicts[0],
-        )
-        with open(self.csv_m1.abs_path, "r", newline="") as file:
+        Path.mkdir(self.folder_path_pathlib, parents=True)
+        with open(self.file_path, "w+", newline="") as file:
+            self.test_manager.add_rows_with_header(
+                fieldnames=self.fieldnames,
+                row=self.test_dicts[0],
+            )
             reader = [line for line in csv.reader(file)]
             self.assertEqual(reader, self.test_data[:2])
-        self.csv_m2.add_rows_with_header(
-            fieldnames=self.fieldnames, rows=self.test_dicts
-        )
-        with open(self.csv_m2.abs_path, "r", newline="") as file:
+
+        with open(self.file_path, "w+", newline="") as file:
+            self.test_manager.add_rows_with_header(
+                fieldnames=self.fieldnames,
+                rows=self.test_dicts,
+            )
             reader = [line for line in csv.reader(file)]
             self.assertEqual(reader, self.test_data)
 
     def test_add_more_rows(self):
-        self.csv_m1.create_file(self.csv_m1.abs_path)
-        self.csv_m1.add_rows_with_header(
-            fieldnames=self.fieldnames,
-            row=self.test_dicts[0],
-        )
-        self.csv_m1.add_more_rows(
-            fieldnames=self.fieldnames,
-            row=self.test_dicts[1],
-        )
-        with open(self.csv_m1.abs_path, "r", newline="") as file:
+        Path.mkdir(self.folder_path_pathlib, parents=True)
+        with open(self.file_path, "w", newline="") as file:
+            writer = csv.writer(file, delimiter=",")
+            writer.writerows(self.test_data[:2])
+        with open(self.file_path, "r", newline="") as file:
+            self.test_manager.add_more_rows(
+                fieldnames=self.fieldnames, row=self.test_dicts[1]
+            )
             reader = [line for line in csv.reader(file)]
             self.assertEqual(reader, self.test_data[:3])
-        self.csv_m1.add_more_rows(
-            fieldnames=self.fieldnames,
-            rows=self.test_dicts[2:],
-        )
-        with open(self.csv_m1.abs_path, "r", newline="") as file:
+
+        with open(self.file_path, "w", newline="") as file:
+            writer = csv.writer(file, delimiter=",")
+            writer.writerows(self.test_data[:3])
+        with open(self.file_path, "r", newline="") as file:
+            self.test_manager.add_more_rows(
+                fieldnames=self.fieldnames, rows=self.test_dicts[2:]
+            )
             reader = [line for line in csv.reader(file)]
             self.assertEqual(reader, self.test_data)
 
     def test_update_file(self):
-        self.csv_m1.create_file(self.csv_m1.abs_path)
-        self.csv_m1.add_rows_with_header(
-            fieldnames=self.fieldnames,
-            rows=self.test_dicts,
-        )
-        self.csv_m1.update_csv(self.fieldnames, "City", 2, "Houston")
-        new_line = ["Jack", "McGinnis", "220 hobo Av.", "Houston", " PA", "9119"]
-        with open(self.csv_m1.abs_path, "r", newline="") as file:
+        Path.mkdir(self.folder_path_pathlib, parents=True)
+        with open(self.file_path, "w", newline="") as file:
+            writer = csv.writer(file, delimiter=",")
+            writer.writerows(self.test_data)
+
+        with open(self.file_path, "r", newline="") as file:
+            self.test_manager.update_csv(self.fieldnames, "City", 2, "Houston")
+            new_line = ["Jack", "McGinnis", "220 hobo Av.", "Houston", " PA", "9119"]
             reader = [line for line in csv.reader(file)]
             self.assertEqual(reader[2], new_line)
 
     def test_scan_path(self):
-        scan_m = CSVManager(
-            "C:\\Ola\\LocalHost\\python-fundamentals-master\\3_Python_OS\\1_EASY\\csv_manager\\new1"
-        )
-        scan_depth_0 = scan_m.scan_path(0, scan_m.abs_path)
+        Path.mkdir(self.folder_path_pathlib, parents=True)
+
+        file1_path = os.path.join(self.folder_path, "read.csv")
+        file2_path = os.path.join(self.folder_path, "to_delete.csv")
+        file3_path = os.path.join(self.folder_path, "with_header.csv")
+        file4_path = os.path.join(self.folder_path, "test_sub", "test_file.csv")
+        Path.mkdir(Path(file4_path).parent)
+        with open(file1_path, "w", newline="") as file1, open(
+            file2_path, "w", newline=""
+        ) as file2, open(file3_path, "w", newline="") as file3, open(
+            file4_path, "w", newline=""
+        ) as file4:
+            pass
+        scan_depth_0 = self.test_manager.scan_path(0, self.folder_path)
         list_paths_depth_0 = [
-            WindowsPath(
-                "C:/Ola/LocalHost/python-fundamentals-master/3_Python_OS/1_EASY/csv_manager/new1/new1.csv"
-            ),
-            WindowsPath(
-                "C:/Ola/LocalHost/python-fundamentals-master/3_Python_OS/1_EASY/csv_manager/new1/new2/new2.csv"
-            ),
+            "test_folder\\read.csv",
+            "test_folder\\to_delete.csv",
+            "test_folder\\with_header.csv",
+            "test_folder\\test_sub\\test_file.csv",
         ]
-        scan_depth_1 = scan_m.scan_path(1, scan_m.abs_path)
-        list_paths_depth_1 = [
-            WindowsPath(
-                "C:/Ola/LocalHost/python-fundamentals-master/3_Python_OS/1_EASY/csv_manager/new1/new2/new2.csv"
-            )
-        ]
-        scan_depth_2 = scan_m.scan_path(2, scan_m.abs_path)
+        scan_depth_1 = self.test_manager.scan_path(1, self.folder_path)
+        list_paths_depth_1 = ["test_folder\\test_sub\\test_file.csv"]
+        scan_depth_2 = self.test_manager.scan_path(2, self.folder_path)
         list_paths_depth_2 = []
         self.assertEqual(scan_depth_0, list_paths_depth_0)
         self.assertEqual(scan_depth_1, list_paths_depth_1)
         self.assertEqual(scan_depth_2, list_paths_depth_2)
 
     def test_delete_file(self):
-        delete_cm = CSVManager(
-            "C:\\Ola\\LocalHost\\python-fundamentals-master\\3_Python_OS\\1_EASY\\csv_manager\\new1",
-            file_name="test_csv.csv",
-        )
-        delete_cm.create_file(delete_cm.abs_path)
-        delete_cm.delete_file(delete_cm.abs_path)
-        self.assertFalse(delete_cm.abs_path.exists())
+        Path.mkdir(self.folder_path_pathlib, parents=True)
+
+        with open(self.file_path, "w", newline="") as file:
+            pass
+        self.test_manager.delete_file(self.file_path)
+        self.assertFalse(os.path.exists(self.file_path))
--- 3_Python_OS/1_EASY/json_manager/test_json_manager.py
+++ 3_Python_OS/1_EASY/json_manager/test_json_manager.py
@@ -1,97 +1,105 @@
 from json_manager import JsonManager
-
+import os
 from unittest import TestCase
 import json
-from pathlib import WindowsPath
+from pathlib import Path
+import shutil
 
 
 class TestJsonManager(TestCase):
     def setUp(self):
-        self.json_m = JsonManager(
-            "C:\\Ola\\LocalHost\\python-fundamentals-master\\3_Python_OS\\1_EASY\\json_manager",
-            file_name="test_file.json",
-        )
+        self.test_manager = JsonManager("test_dir", file_name="test_file.json")
+        self.file_path = os.path.join(os.getcwd(), "test_dir", "test_file.json")
+        self.folder_path = os.path.join(os.getcwd(), "test_dir")
+        self.folder_path_pathlib = Path(self.folder_path)
         self.test_dict = {
-            "first": {"1stfolder": {"2ndfolder": {"3rdfolder": {"file": "file.csv"}}}},
-            "second": {"1stfolder": {"2nd folder": {"file": "file.csv"}}},
+            "first": {"1stfolder": {"2ndfolder": {"3rdfolder": {"file": "file.json"}}}},
+            "second": {"1stfolder": {"2nd folder": {"file": "file.json"}}},
         }
 
     def tearDown(self):
-        if self.json_m.abs_path.exists():
-            self.json_m.abs_path.unlink()
+        if os.path.exists(self.folder_path):
+            shutil.rmtree(self.folder_path)
+        elif os.path.exists(self.file_path):
+            os.remove(self.file_path)
 
     def test_create_file(self):
-        file_path = self.json_m.path.joinpath("test_file.json")
-        self.json_m.create_file(self.json_m.abs_path)
-        self.assertEqual(self.json_m.abs_path, file_path)
+        Path.mkdir(self.folder_path_pathlib, parents=True)
+        self.test_manager.create_file(self.file_path)
+        self.assertTrue(os.path.exists(self.file_path))
 
     def test_read_file(self):
-        self.json_m.create_file(self.json_m.abs_path)
-        with open(self.json_m.abs_path, "w") as file:
+        Path.mkdir(self.folder_path_pathlib, parents=True)
+        with open(self.file_path, "w") as file:
             json.dump(self.test_dict, file, indent=2)
-        loaded = self.json_m.read_file()
+        loaded = self.test_manager.read_file()
         self.assertEqual(self.test_dict, loaded)
 
     def test_set_data(self):
-        self.json_m.create_file(self.json_m.abs_path)
-        self.json_m.set_data(self.test_dict)
-        with open(self.json_m.abs_path, "r") as file:
+        Path.mkdir(self.folder_path_pathlib, parents=True)
+        with open(self.file_path, "w") as file:
+            pass
+        self.test_manager.set_data(self.test_dict)
+        with open(self.file_path, "r") as file:
             loaded = json.load(file)
             self.assertEqual(self.test_dict, loaded)
 
     def test_update_file(self):
         new_data = {"first": ["test_name1", "test_name2"], "second": "file"}
-        self.json_m.create_file(self.json_m.abs_path)
-        self.json_m.set_data(self.test_dict)
-        self.json_m.update_file(new_data)
-        with open(self.json_m.abs_path, "r") as file:
+        Path.mkdir(self.folder_path_pathlib, parents=True)
+        with open(self.file_path, "w") as file:
+            json.dump(self.test_dict, file, indent=2)
+        self.test_manager.update_file(new_data)
+        with open(self.file_path, "r") as file:
             loaded = json.load(file)
             self.assertEqual(new_data, loaded)
 
     def test_append_data(self):
-        self.json_m.create_file(self.json_m.abs_path)
-        self.json_m.set_data(self.test_dict)
+        Path.mkdir(self.folder_path_pathlib, parents=True)
+        with open(self.file_path, "w") as file:
+            json.dump(self.test_dict, file, indent=2)
         appended_dict = {"third": {"1": {"2": {"3": "file"}}}}
-        self.json_m.append_data(appended_dict)
+        self.test_manager.append_data(appended_dict)
         dict_with_append = {
-            "first": {"1stfolder": {"2ndfolder": {"3rdfolder": {"file": "file.csv"}}}},
-            "second": {"1stfolder": {"2nd folder": {"file": "file.csv"}}},
+            "first": {"1stfolder": {"2ndfolder": {"3rdfolder": {"file": "file.json"}}}},
+            "second": {"1stfolder": {"2nd folder": {"file": "file.json"}}},
             "third": {"1": {"2": {"3": "file"}}},
         }
-        with open(self.json_m.abs_path, "r") as file:
+        with open(self.file_path, "r") as file:
             loaded = json.load(file)
             self.assertEqual(dict_with_append, loaded)
 
     def test_scan_path(self):
-        scan_m = JsonManager(
-            "C:\\Ola\\LocalHost\\python-fundamentals-master\\3_Python_OS\\1_EASY\\json_manager\\test_folder"
-        )
-        scan_depth_0 = scan_m.scan_path(0, scan_m.path, ext=".json")
+        Path.mkdir(self.folder_path_pathlib, parents=True)
+
+        file1_path = os.path.join(self.folder_path, "read.json")
+        file2_path = os.path.join(self.folder_path, "to_delete.json")
+        file3_path = os.path.join(self.folder_path, "with_header.json")
+        file4_path = os.path.join(self.folder_path, "test_sub", "test_file.json")
+        Path.mkdir(Path(file4_path).parent)
+
+        with open(file1_path, "w") as file1, open(file2_path, "w") as file2, open(
+            file3_path, "w"
+        ) as file3, open(file4_path, "w") as file4:
+            pass
+        scan_depth_0 = self.test_manager.scan_path(0, self.folder_path, ext=".json")
         list_paths_depth_0 = [
-            WindowsPath(
-                "C:/Ola/LocalHost/python-fundamentals-master/3_Python_OS/1_EASY/json_manager/test_folder/test_file1.json"
-            ),
-            WindowsPath(
-                "C:/Ola/LocalHost/python-fundamentals-master/3_Python_OS/1_EASY/json_manager/test_folder/inner_folder/test_file2.json"
-            ),
+            "test_dir\\read.json",
+            "test_dir\\to_delete.json",
+            "test_dir\\with_header.json",
+            "test_dir\\test_sub\\test_file.json",
         ]
-        scan_depth_1 = scan_m.scan_path(1, scan_m.path, ext=".json")
-        list_paths_depth_1 = [
-            WindowsPath(
-                "C:/Ola/LocalHost/python-fundamentals-master/3_Python_OS/1_EASY/json_manager/test_folder/inner_folder/test_file2.json"
-            )
-        ]
-        scan_depth_2 = scan_m.scan_path(2, scan_m.path, ext=".json")
+        scan_depth_1 = self.test_manager.scan_path(1, self.folder_path, ext=".json")
+        list_paths_depth_1 = ["test_dir\\test_sub\\test_file.json"]
+        scan_depth_2 = self.test_manager.scan_path(2, self.folder_path, ext=".json")
         list_paths_depth_2 = []
         self.assertEqual(scan_depth_0, list_paths_depth_0)
         self.assertEqual(scan_depth_1, list_paths_depth_1)
         self.assertEqual(scan_depth_2, list_paths_depth_2)
 
     def test_delete_file(self):
-        delete_cm = JsonManager(
-            "C:\\Ola\\LocalHost\\python-fundamentals-master\\3_Python_OS\\1_EASY\\json_manager\\test_folder",
-            file_name="test_file2.json",
-        )
-        delete_cm.create_file(delete_cm.abs_path)
-        delete_cm.delete_file(delete_cm.abs_path)
-        self.assertFalse(delete_cm.abs_path.exists())
+        Path.mkdir(self.folder_path_pathlib, parents=True)
+        with open(self.file_path, "w", newline="") as file:
+            pass
+        self.test_manager.delete_file(self.file_path)
+        self.assertFalse(os.path.exists(self.file_path))
