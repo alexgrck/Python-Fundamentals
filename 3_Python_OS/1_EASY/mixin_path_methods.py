abc017a93f3e538384ea779271577ae3ba88fef7
Aleksandra Gierczak <aleksandra.gierczak09@gmail.com>
2023-03-17 22:35:43 +0100

    two methods modified

--- 3_Python_OS/1_EASY/mixin_path_methods.py
+++ 3_Python_OS/1_EASY/mixin_path_methods.py
@@ -6,11 +6,11 @@ class MixinPathMethods:
     def scan_path(self, depth, path, ext=".csv"):
         if depth > 0:
             str_depth = "*" + (r"\*" * depth) + ext
-            return [os.path.relpath(str(file)) for file in Path(path).glob(str_depth)]
-        return [os.path.relpath(str(file)) for file in Path(path).glob(f"**/*{ext}")]
+            return [file for file in Path(path).glob(str_depth)]
+        return [file for file in Path(path).glob(f"**/*{ext}")]
 
     def create_file(self, file_name):
-        with open(file_name, "w") as file:
+        with open(file_name, "w"):
             pass
 
     def delete_file(self, abs_path):
