3a966b55e025730c00c70cd178ed54f24348cc27
Aleksandra Gierczak <aleksandra.gierczak09@gmail.com>
2023-04-18 22:31:57 +0200

    methods finished

--- 3_Python_OS/2_MEDIUM/archive/archive.py
+++ 3_Python_OS/2_MEDIUM/archive/archive.py
@@ -1,40 +1,37 @@
 import os
-import sys
+
 from shutil import make_archive, unpack_archive
 from zipfile import ZipFile
 
 
 class ArchiveMixin:
-    def make_arch(self, path, arch_name, dir_name):
-        archive_name = os.path.expanduser(os.path.join(path, "~", arch_name))
-        platforms = {"Windows": "win32", "Linux": "linux", "macOS": "darwin"}
-        if sys.platform.startswith("linux"):
-            make_archive(archive_name, "gztar", root_dir=path, base_dir=dir_name)
-        elif sys.platform.startswith("win32"):
-            make_archive(archive_name, "zip", root_dir=path, base_dir=dir_name)
+    def make_arch(self, path, arch_name):
+        op_systems = {"posix": "tar.gz", "nt": "zip"}
+        os.chdir(os.path.dirname(path))
+        make_archive(
+            arch_name,
+            op_systems.get(os.name, "tar.gz"),
+            base_dir=os.path.basename(path),
+        )
 
-    # https://docs.python.org/3/library/sys.html#sys.platform
-    # macOS
-    # linuxowe, macosy - systemy unixowe
-    # inny wybór rozszerzenia
-    # czyściej, słownik
-    # nie rozumiem expanduser
-    # skoro podajemy arch_name, to czemu folder ma samą tyldę?
-    # przetestować w csv i json
-
-    def add_to_archive(self, zip_file_path, file_path):
-        with ZipFile(zip_file_path, "a") as archive:
-            archive.write(file_path)
-        # zipfile?
-        # pass
+    def add_to_archive(self, archive_path, file_path):
+        with ZipFile(archive_path, "a") as archive:
+            archive.write(file_path, os.path.basename(file_path))
 
     def extract_archive(self, archive_path, extract_dir, archive_format):
         unpack_archive(archive_path, extract_dir, archive_format)
 
-    def remove_from_archive(self):
-        pass
-
-
-# zrobić pliki i klasę testową i próbować
-
-# w sumie to takiego zipa tworzymy
+    def remove_from_archive(self, archive_path, file_to_delete):
+        temp_path = os.path.join(os.path.dirname(archive_path), "temp")
+        temp_file = os.path.join(temp_path, file_to_delete)
+        os.mkdir(temp_path)
+        with ZipFile(archive_path, "r") as archive:
+            archive.extractall(temp_path)
+        arch_name = os.path.basename(os.path.splitext(archive_path)[0])
+        os.remove(archive_path)
+        os.remove(temp_file)
+        os.chdir(temp_path)
+        with ZipFile(archive_path, "a") as archive:
+            for root, dirs, files in os.walk(temp_path):
+                for file in files:
+                    archive.write(os.path.relpath(os.path.join(root, file)))
