1df899b42ed46db8aeff22a83fc5ba6f226351af
Aleksandra Gierczak <aleksandra.gierczak09@gmail.com>
2023-04-24 14:43:45 +0200

    modifications in some methods

--- 3_Python_OS/1_EASY/csv_manager/csv_manager_pandas.py
+++ 3_Python_OS/1_EASY/csv_manager/csv_manager_pandas.py
@@ -1,32 +1,33 @@
-from pathlib import Path
+import os
 import sys
+from pathlib import Path
+
 import pandas as pd
-
-sys.path.append("C:\\Ola\\LocalHost\\python-fundamentals-master\\3_Python_OS\\1_EASY")
-
-from path_methods import MixinPathMethods
+from mixin_path_methods import MixinPathMethods
 
 
 class CSVManager(MixinPathMethods):
-    def __init__(self, path_string, file_name=""):
-        self.path = Path(path_string)
-        self.file_name_path = self.path.joinpath(file_name)
-        self.abs_path = (
-            self.file_name_path
-            if self.file_name_path.is_absolute()
-            else Path.cwd().joinpath(self.file_name_path)
-        )
+    def __init__(self, *path_elements, file_name=""):
+        self.abs_path = Path(os.path.join(*path_elements, file_name)).absolute()
 
     def read_csv(self):
         return pd.read_csv(self.abs_path)
 
     def set_data(self, columns, data):
-        df = pd.DataFrame(data, columns=columns)
+        if isinstance(data, dict) or len(data) == 1:
+            main_data = [data]
+        else:
+            main_data = data
+        df = pd.DataFrame(main_data, columns=columns)
         df.to_csv(self.abs_path, index=False)
 
     def add_rows(self, columns, data):
+        if isinstance(data, dict) or len(data) == 1:
+            main_data = [data]
+        else:
+            main_data = data
         file_data = pd.read_csv(self.abs_path)
-        df = pd.DataFrame(data, columns=columns)
+        df = pd.DataFrame(main_data, columns=columns)
         final_df = pd.concat([file_data, df], ignore_index=True)
         final_df.to_csv(self.abs_path, index=False)
 
