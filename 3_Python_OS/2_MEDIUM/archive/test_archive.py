2c91e9e34d073c25adfd1e65a5a75c27439bfb6f
Aleksandra Gierczak <aleksandra.gierczak09@gmail.com>
2023-05-18 10:38:06 +0200

    all methods fixed

--- 3_Python_OS/2_MEDIUM/archive/test_archive.py
+++ 3_Python_OS/2_MEDIUM/archive/test_archive.py
@@ -2,101 +2,85 @@ import os
 import shutil
 from pathlib import Path
 from unittest import TestCase
-
+import stat
+from zipfile import ZipFile
 from test_class import TestingArchiveMixin
 
 
 class TestArchive(TestCase):
     def setUp(self):
-        # stworzyć jeden folder, w którym będę miała foldery do pakowania i wypakowywania,
-        # żeby później w teardown class usuwać tylko ten folder
         self.test_archive = TestingArchiveMixin("main", "to_be_archived")
-        self.test_folder_path = Path.cwd().joinpath("main", "to_be_archived")
-        # Path(
-        #     os.path.join(os.getcwd(), "main", "to_be_archived")
-        # )
-        Path.mkdir(self.test_folder_path, parents=True)
-        # os.chdir(str(self.test_folder_path.parent))
-        # Path(os.path.join(os.getcwd(), "main", "test.zip"))
+        self.main_dir = os.path.join(os.getcwd(), "main")
+        self.to_be_archived = os.path.join(self.main_dir, "to_be_archived")
+        Path(self.to_be_archived).mkdir(parents=True)
         self.test_files = ["test1.txt", "test2.txt"]
         for file in self.test_files:
-            # Path(os.path.join(os.getcwd(), "main", "to_be_archived", file)).touch()
-            self.test_folder_path.joinpath(file).touch()
-        self.extract_dir = self.test_folder_path.parent
-        self.archive_path = self.extract_dir.joinpath("test.zip")
-        self.file_to_add = self.test_folder_path.joinpath("test3.txt")
-        # zmockować to
+            Path(self.to_be_archived).joinpath(file).touch()
+        self.archive_path = os.path.join(self.main_dir, "test.zip")
+        self.extract_dir = os.path.join(self.main_dir, "unpacked")
+        Path(self.extract_dir).mkdir()
+        self.file_to_add = os.path.join(self.to_be_archived, "test3.txt")
+        self.extracted1 = os.path.join(self.extract_dir, "to_be_archived", "test1.txt")
+        self.extracted2 = os.path.join(self.extract_dir, "to_be_archived", "test2.txt")
+        self.extracted3 = os.path.join(self.extract_dir, "test3.txt")
         self.op_systems = {"posix": "tar.gz", "nt": "zip"}
 
     def tearDown(self):
-        # os.chdir(os.getcwd())
-        shutil.rmtree(self.test_folder_path.parent)
-        # shutil.rmtree(Path.cwd().joinpath("main"))
-
-    # self.test_folder_path.rmdir()
-    # if os.path.exists(self.test_folder_path):
-    #     shutil.rmtree(self.test_folder_path)
-    # if os.path.exists(self.extract_dir):
-    # os.chdir(os.getcwd())
-    # shutil.rmtree(self.extract_dir)
-
-    # if os.path.exists(self.archive_path):
-    #     os.remove(self.archive_path)
+        os.chdir(os.path.dirname(os.getcwd()))
+        shutil.rmtree(self.main_dir)
 
     def test_create_archive(self):
-        print("CREATING")
-        self.test_archive.make_arch(self.test_folder_path, "test")
-        # self.assertTrue(os.path.exists(self.archive_path))
-        print(self.assertTrue(self.archive_path.exists()))
+        self.test_archive.make_arch(self.to_be_archived, "test")
+        self.assertTrue(os.path.exists(self.archive_path))
+
         shutil.unpack_archive(self.archive_path, self.extract_dir, "zip")
-        extracted1 = self.extract_dir.joinpath("to_be_archived", "test1.txt")
-        extracted2 = self.extract_dir.joinpath("to_be_archived", "test2.txt")
-        print(extracted1)
-        print(extracted2)
-        # Path(os.path.join(self.extract_dir, "to_be_archived", "test1.txt"))
-        # extracted2 = Path(os.path.join(self.extract_dir, "to_be_archived", "test2.txt"))
-        print(self.assertTrue(os.path.exists(extracted1)))
-        print(self.assertTrue(os.path.exists(extracted2)))
+        self.assertTrue(os.path.exists(self.extracted1))
+        self.assertTrue(os.path.exists(self.extracted2))
 
-    # def test_add_to_archive(self):
-    #     shutil.make_archive(
-    #         "test",
-    #         self.op_systems.get(os.name, "tar.gz"),
-    #         base_dir=os.path.basename(self.test_folder_path),
-    #     )
-    #     self.file_to_add.touch()
-    #     self.test_archive.add_to_archive(self.archive_path, self.file_to_add)
-    #     shutil.unpack_archive(self.archive_path, self.extract_dir, "zip")
-    #     extracted1 = Path(os.path.join(self.extract_dir, "test_folder", "test1.txt"))
-    #     extracted2 = Path(os.path.join(self.extract_dir, "test_folder", "test2.txt"))
-    #     extracted3 = Path(os.path.join(self.extract_dir, "test3.txt"))
-    #     self.assertTrue(os.path.exists(extracted1))
-    #     self.assertTrue(os.path.exists(extracted2))
-    #     self.assertTrue(os.path.exists(extracted3))
+    def test_add_to_archive(self):
+        os.chdir(self.main_dir)
+        shutil.make_archive(
+            "test",
+            self.op_systems.get(os.name, "tar.gz"),
+            base_dir=os.path.basename(self.to_be_archived),
+        )
+        Path(self.file_to_add).touch()
 
-    # def test_extract_archive(self):
-    #     shutil.make_archive(
-    #         "test",
-    #         self.op_systems.get(os.name, "tar.gz"),
-    #         base_dir=os.path.basename(self.test_folder_path),
-    #     )
-    #     self.test_archive.extract_archive(self.archive_path, self.extract_dir, "zip")
-    #     extracted1 = Path(os.path.join(self.extract_dir, "test_folder", "test1.txt"))
-    #     extracted2 = Path(os.path.join(self.extract_dir, "test_folder", "test2.txt"))
-    #     self.assertTrue(os.path.exists(extracted1))
-    #     self.assertTrue(os.path.exists(extracted2))
+        self.test_archive.add_to_archive(self.archive_path, self.file_to_add)
 
-    # def test_remove_from_archive(self):
-    #     shutil.make_archive(
-    #         "test",
-    #         self.op_systems.get(os.name, "tar.gz"),
-    #         base_dir=os.path.basename(self.test_folder_path),
-    #     )
-    #     self.test_archive.remove_from_archive(
-    #         self.archive_path, "test_folder/test2.txt"
-    #     )
-    #     shutil.unpack_archive(self.archive_path, self.extract_dir, "zip")
-    #     extracted1 = Path(os.path.join(self.extract_dir, "test_folder", "test1.txt"))
-    #     extracted2 = Path(os.path.join(self.extract_dir, "test_folder", "test2.txt"))
-    #     self.assertTrue(os.path.exists(extracted1))
-    #     self.assertFalse(os.path.exists(extracted2))
+        shutil.unpack_archive(self.archive_path, self.extract_dir, "zip")
+        self.assertTrue(os.path.exists(self.extracted1))
+        self.assertTrue(os.path.exists(self.extracted2))
+        self.assertTrue(os.path.exists(self.extracted3))
+
+    def test_extract_archive(self):
+        os.chdir(self.main_dir)
+        shutil.make_archive(
+            "test",
+            self.op_systems.get(os.name, "tar.gz"),
+            base_dir=os.path.basename(self.to_be_archived),
+        )
+
+        self.test_archive.extract_archive(self.archive_path, self.extract_dir, "zip")
+
+        self.assertTrue(os.path.exists(self.extracted1))
+        self.assertTrue(os.path.exists(self.extracted2))
+
+    def test_remove_from_archive(self):
+        os.chdir(self.main_dir)
+        shutil.make_archive(
+            "test",
+            self.op_systems.get(os.name, "tar.gz"),
+            base_dir=os.path.basename(self.to_be_archived),
+        )
+
+        self.test_archive.remove_from_archive(
+            self.archive_path,
+            os.path.join(os.path.basename(self.to_be_archived), "test2.txt"),
+        )
+
+        shutil.unpack_archive(self.archive_path, self.extract_dir, "zip")
+        extracted1 = Path(os.path.join(self.extract_dir, "to_be_archived", "test1.txt"))
+        extracted2 = Path(os.path.join(self.extract_dir, "to_be_archived", "test2.txt"))
+        self.assertTrue(os.path.exists(extracted1))
+        self.assertFalse(os.path.exists(extracted2))
